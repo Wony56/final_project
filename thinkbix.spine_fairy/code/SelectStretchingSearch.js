e902b02bb8f1be8297b131e9665f4efd585f7cfc
LeeSuKyeong <dltnrud987@gmail.com>
2019-10-22 18:07:39 +0900

    Fix layout so that db is called

--- thinkbix.spine_fairy/code/SelectStretchingSearch.js
+++ thinkbix.spine_fairy/code/SelectStretchingSearch.js
@@ -3,14 +3,19 @@ var console = require('console')
 module.exports.function = function SelectStretchingSearch (place, part) {
   // const fakeData = require("./data/SelectData.js");
   console.log("잘거면 숙박비 내라");
-  var response = http.getUrl('http://52.79.199.186:8080/find/all',{format:'json'}).datas;
-  
-  console.log(response);
+  url = encodeURI('http://52.79.199.186:8080/find?part='+part);
+  var response = http.getUrl(url,{format:'json'}).datas;
+
+  // console.log(response);
   
   // const console = require("console");
   let stretchingSel = [];
   for(let i = 0; i < response.length; i++){
-      stretchingSel.push(response[i]);
+      if(response[i] != null){
+        stretchingSel.push(response[i]);
+      }
+  
   }
+  console.log(stretchingSel)
   return stretchingSel;
 }
