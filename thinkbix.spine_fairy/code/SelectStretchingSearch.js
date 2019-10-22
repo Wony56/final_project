37fa5452a7e4efdbb5b5a03fbb7dc68358d24624
juyeonsg <juyeonsg@gmail.com>
2019-10-22 09:10:12 +0900

    Fix model and concept in bixby

--- thinkbix.spine_fairy/code/SelectStretchingSearch.js
+++ thinkbix.spine_fairy/code/SelectStretchingSearch.js
@@ -1,10 +1,16 @@
+var http=require('http')
+var console = require('console')
 module.exports.function = function SelectStretchingSearch (place, part) {
-  const fakeData = require("./data/SelectData.js");
-  const console = require("console");
-
+  // const fakeData = require("./data/SelectData.js");
+  console.log("잘거면 숙박비 내라");
+  var response = http.getUrl('http://52.79.199.186:8080/find/all',{format:'json'}).datas;
+  
+  console.log(response);
+  
+  // const console = require("console");
   let stretchingSel = [];
-  for(let i = 0; i < fakeData.length; i++){
-      stretchingSel.push(fakeData[i]);
+  for(let i = 0; i < response.length; i++){
+      stretchingSel.push(response[i]);
   }
   return stretchingSel;
 }
