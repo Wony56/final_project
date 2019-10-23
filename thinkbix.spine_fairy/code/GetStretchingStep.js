33a3fd662a6932a14d771536bc9a44cfa53389e9
LeeSuKyeong <dltnrud987@gmail.com>
2019-10-23 19:35:58 +0900

    Add Pagination

--- thinkbix.spine_fairy/code/GetStretchingStep.js
+++ thinkbix.spine_fairy/code/GetStretchingStep.js
@@ -1,11 +1,27 @@
 var console = require('console')
-module.exports.function = function getStretchingStep (stretchingSel) {
-  console.log(stretchingSel.processList[0])
-  console.log("aa")
-  let stretchingStep =stretchingSel.processList[0];
+module.exports.function = function getStretchingStep (stretchingSel,stretchingStep) {
+  // console.log(stretchingSel.processList[step])
+  // console.log("aa")
+  // let stretchingStep =stretchingSel.processList[step];
+  // var idx =0;
+  // if(step == null){
+  //   idx = 0
+  // }else{
+  //   idx = step+1
+  // }
+  // console.log(idx)
+  // console.log(stretchingSel.processList[idx])
+  var idx = 0
+  if(stretchingStep==null){
+    idx = 0;
+  }else{
+    idx = stretchingStep.idx +1
+  }
+  console.log(idx)
   return {
-    imgAddress : stretchingSel.processList[0].imgAddress,
-    imgPath : stretchingSel.processList[0].imgPath,
-    description : stretchingSel.processList[0].description
+    imgAddress : stretchingSel.processList[idx].imgAddress,
+    imgPath : stretchingSel.processList[idx].imgPath,
+    description : stretchingSel.processList[idx].description,
+    idx : idx
   }
 }
