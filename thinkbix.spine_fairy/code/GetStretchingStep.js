aebbace8b726414cdde983192906321d1f83080c
MoonYongseong <forever9882@gmail.com>
2019-10-23 17:32:58 +0900

    Implement Youtube API

--- thinkbix.spine_fairy/code/GetStretchingStep.js
+++ thinkbix.spine_fairy/code/GetStretchingStep.js
@@ -1,7 +1,5 @@
 var console = require('console')
 module.exports.function = function getStretchingStep (stretchingSel) {
-  console.log(stretchingSel.processList[0])
-  console.log("aa")
   let stretchingStep =stretchingSel.processList[0];
   return {
     imgAddress : stretchingSel.processList[0].imgAddress,
