2104840e8f2994db6d2b6e90b2b8b55fac367a3f
이석원 <blackmonkey9256@gmail.com>
2019-10-24 15:26:04 +0900

    Implement restriction for adminpage and Fix find controller

--- backend/routers/router.js
+++ backend/routers/router.js
@@ -1,14 +1,26 @@
 import express from "express";
 import routes from "../routes";
 import { home, postUpload, postUploadMany, getView, getAllStretchings, getStretchings } from "../controllers/dataController";
-import { uploadImage } from "../middlewares";
+import { uploadImage, onlyAdmin, onlyPrivate, onlyPublic } from "../middlewares";
+import { getJoin, postJoin, postLogin, getLogin, logout } from "../controllers/userController";
 
 const router = express.Router();
 
-router.get(routes.home,  home);
+router.get(routes.home, onlyAdmin, home);
+
+//Join
+router.get(routes.join, onlyPublic, getJoin);
+router.post(routes.join, onlyPublic, postJoin, postLogin);
+
+//Login
+router.get(routes.login, onlyPublic, getLogin);
+router.post(routes.login, onlyPublic, postLogin);
+
+//Logout
+router.get(routes.logout, onlyPrivate, logout);
 
 // Data Upload
-router.post(routes.upload, uploadImage, postUpload);
+router.post(routes.upload, onlyAdmin, uploadImage, postUpload);
 router.post(routes.uploadMany, postUploadMany);
 
 //Search Datas
