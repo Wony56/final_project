d8aaa8d877b4aa09e89b78b69e3a4c57488a4840
MoonYongseong <forever9882@gmail.com>
2019-10-24 10:59:00 +0900

    Fix project name

--- thinkbix.spine_fairy/code/SelectStretchingSearch.js
+++ thinkbix.spine_fairy/code/SelectStretchingSearch.js
@@ -1,20 +1,16 @@
 var http=require('http')
 var console = require('console')
+var config = require('config')
 module.exports.function = function SelectStretchingSearch (place, part) {
-  // const fakeData = require("./data/SelectData.js");
-  console.log("잘거면 숙박비 내라");
-  url = encodeURI('http://52.79.199.186:8080/find?part='+part);
+  url = encodeURI(config.get('remoteURL')+'find?part='+part);
   var response = http.getUrl(url,{format:'json'}).datas;
 
-  // console.log(response);
-  
-  // const console = require("console");
   let stretchingSel = [];
   for(let i = 0; i < response.length; i++){
       if(response[i] != null){
+        response[i].index = 0;
         stretchingSel.push(response[i]);
       }
-  
   }
   console.log(stretchingSel)
   return stretchingSel;
