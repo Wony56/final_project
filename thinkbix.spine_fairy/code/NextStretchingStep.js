a61a0d97f8ac0e7c3bf5d9a97d490446fa180c52
juyeonsg <juyeonsg@gmail.com>
2019-10-27 21:42:19 +0900

    Fix index error in layout

--- thinkbix.spine_fairy/code/NextStretchingStep.js
+++ thinkbix.spine_fairy/code/NextStretchingStep.js
@@ -1,14 +1,12 @@
 var console = require('console')
-module.exports.function = function NextStretchingStep (stretchingSel,stretchingStep) {
-
+module.exports.function = function StartStretching (stretchingSel,stretchingStep) {
   var idx = 0
   if(stretchingStep==null){
     idx = 0;
   }else{
     idx = stretchingStep.idx +1
   }
-  console.log(idx)
-  return {
+  return{
     imgAddress : stretchingSel.processList[idx].imgAddress,
     imgPath : stretchingSel.processList[idx].imgPath,
     description : stretchingSel.processList[idx].description,
