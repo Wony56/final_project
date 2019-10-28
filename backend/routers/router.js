53d70fb515c083f6767844c2a5d1aaff3361988b
이석원 <blackmonkey9256@gmail.com>
2019-10-28 11:45:17 +0900

    Implement DataList page and Detail page

--- backend/routers/router.js
+++ backend/routers/router.js
@@ -1,6 +1,6 @@
 import express from "express";
 import routes from "../routes";
-import { home, postUpload, postUploadMany, getView, getAllStretchings, getStretchings, getStretchingsByViews } from "../controllers/dataController";
+import { home, postUpload, postUploadMany, getView, getAllStretchings, getStretchings, getStretchingsByViews, getAllStretchingsForAdmin, getDetail, getEdit, postEdit, getDelete } from "../controllers/dataController";
 import { uploadImage, onlyAdmin, onlyPrivate, onlyPublic } from "../middlewares";
 import { getJoin, postJoin, postLogin, getLogin, logout } from "../controllers/userController";
 
@@ -23,12 +23,21 @@ router.get(routes.logout, onlyPrivate, logout);
 router.post(routes.upload, onlyAdmin, uploadImage, postUpload);
 router.post(routes.uploadMany, postUploadMany);
 
+// Edit Data
+router.get(routes.edit(), onlyAdmin, getEdit);
+router.post(routes.edit(), onlyAdmin, postEdit);
+
+// Delete Data
+router.get(routes.delete(), onlyAdmin, getDelete);
+
 //Search Datas
 router.get(routes.findAll, getAllStretchings);
 router.get(routes.findByView, getStretchingsByViews);
 router.get(routes.find, getStretchings);
+router.get(routes.findList, getAllStretchingsForAdmin);
 
 //Views
-router.get(routes.view, getView);
+router.get(routes.view(), getView);
+router.get(routes.detail(), getDetail);
 
 export default router;
--- backend/routes.js
+++ backend/routes.js
@@ -11,11 +11,18 @@ const UPLOAD_MANY = "/upload/many";
 
 // Data Search
 const FIND_ALL = "/find/all";
+const FIND_LIST = "/find/list";
 const FIND_BY_VIEWS = "/find/view";
 const FIND = "/find";
 
+// Update data
+const EDIT = "/edit/:id";
+const DELETE = "/delete/:id";
+
 // Data view
-const VIEW = "/view/:id"
+const VIEW = "/view/:id";
+
+const DETAIL = "/detail/:id";
 
 const routes = {
     home: HOME,
@@ -25,9 +32,37 @@ const routes = {
     upload: UPLOAD,
     uploadMany: UPLOAD_MANY,
     findAll: FIND_ALL,
+    findList: FIND_LIST,
     findByView: FIND_BY_VIEWS,
     find: FIND,
-    view: VIEW
+    view: id => {
+        if(id){
+            return `/view/${id}`;
+        }else{
+            return  VIEW;
+        }
+    },
+    detail:id => {
+        if(id){
+            return `/detail/${id}`;
+        }else{
+            return DETAIL;
+        }
+    },
+    edit: id => {
+        if(id){
+            return `/edit/${id}`;
+        }else{
+            return EDIT;
+        }
+    },
+    delete: id => {
+        if(id){
+            return `/delete/${id}`;
+        }else{
+            return DELETE;
+        }
+    } 
 };
 
 export default routes;
