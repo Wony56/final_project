84f1a5bfefb9c4120c13b0b75a9f519e4970d995
이석원 <blackmonkey9256@gmail.com>
2019-11-04 11:15:16 +0900

    Modify logout

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -18,6 +18,12 @@ export default {
   recommend() {
     return axios.get(`${apiUrl}/search/recommend`);
   },
+  editProfile(params) {
+    return axios.post(`${apiUrl}/edit-profile`, params);
+  },
+  changePassword(params) {
+    return axios.post(`${apiUrl}/change-password`, params);
+  },
   getAllStretchings() {
     return axios.get(`${apiUrl}/search/all`);
   },
